81dea85d536781d3aa57810196de72ada4a5f092
simplymathematics <charliemeyers@gmail.com>
2018-09-10 01:01:31 -0400

    typo when adding head line without testing :"

--- Homework/2/dataframe.R
+++ Homework/2/dataframe.R
@@ -4,4 +4,4 @@
 install.packages("RMySQL")
 library(RMySQL)
 database <- dbConnect(MySQL(), user='user', password=localuserpassword, dbname='movies', host='localhost')
-head(movies, 10)
+head(database, 10)
